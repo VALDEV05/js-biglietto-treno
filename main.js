844130c3d2748b3fba4031bfc198c0cd3e366b54
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-11 13:46:33 +0200

    Ticket price calculation

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ il prezzo del biglietto è definito in base ai km(0.21 € al km) va applicato u
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca. */
 
+//step 1
 // chiedo il numero dei chilometri che l'utente dovrà percorrere
-const numKm = Number(prompt('Quanti chilometri dovrai percorrere?'));
-console.log("l'utente dovrà percorrere", String(numKm), "km");
-const userAge = Number(prompt('Quanti anni hai?'));
-console.log("l'utente ha", String(userAge), 'anni');
+const num_km = Number(prompt('Quanti chilometri dovrai percorrere?'));
+console.log("l'utente dovrà percorrere", String(num_km), "km");
+const user_age = Number(prompt('Quanti anni hai?'));
+console.log("l'utente ha", String(user_age), 'anni');
+
+//step 2
+//calcolo prezzo biglietto
+const ticket_price = num_km * 0.21
+console.log('Il costo del biglietto e di euro: ', String(ticket_price));
+
+
+
+
+//step 3
+//mostra il prezzo finale
