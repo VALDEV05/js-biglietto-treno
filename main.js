fa0eca5caa1283940cd22d294bbfea2f60c9cd7b
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-11 14:07:05 +0200

    Discount calculation and the new ticket price based on age

--- main.js
+++ main.js
@@ -6,18 +6,41 @@ Questo richiederà un minimo di ricerca. */
 
 //step 1
 // chiedo il numero dei chilometri che l'utente dovrà percorrere
-const num_km = Number(prompt('Quanti chilometri dovrai percorrere?'));
+const num_km = Number(prompt('Quanti chilometri dovrai percorrere?')).toFixed(0);
 console.log("l'utente dovrà percorrere", String(num_km), "km");
 const user_age = Number(prompt('Quanti anni hai?'));
 console.log("l'utente ha", String(user_age), 'anni');
 
 //step 2
 //calcolo prezzo biglietto
-const ticket_price = num_km * 0.21
-console.log('Il costo del biglietto e di euro: ', String(ticket_price));
+const ticket_price = (num_km * 0.21).toFixed(2);
 
 
+const discount_20 = ((ticket_price / 100) * 20).toFixed(2);
+const discount_40 = ((ticket_price / 100) * 40).toFixed(2);
+const ticket_20 = (ticket_price - discount_20).toFixed(2)
+const ticket_40 = (ticket_price - discount_40).toFixed(2)
 
 
-//step 3
+//step 3 
+//calcolo sconti
+//i < 18 sconto 20%
+// i > 65 sconto 45%
+//capisci età utente
+
+if (user_age < 18) {
+    console.log('applica uno sconto del 20%')
+    console.log('lo sconto è pari a:', discount_20, String('€'));
+    console.log('il costo del biglietto scontato è:', ticket_20, String('€'));
+}
+else if (user_age > 65) {
+    console.log('applica uno sconto del 40%')
+    console.log('lo sconto è pari a:', discount_40, String('€'));
+    console.log('il costo del biglietto scontato è:', ticket_40, String('€'))
+}
+else {
+    console.log('non è possibile applicare nessuno sconto')
+    console.log('Il costo del biglietto e di euro: ', String(ticket_price));
+}
+//step 4
 //mostra il prezzo finale
